3d5d4752389b4a6644d3d45a1472fd0e0ebc1e39
Soheil Hajian <soheil.hajian@outlook.com>
2024-08-28 09:00:33 +0200

    Improve app. Fix tax definition.

--- immo_rechner/app/app.py
+++ immo_rechner/app/app.py
@@ -2,44 +2,37 @@ import numpy as np
 from dash import Dash, html, dcc, callback, Output, Input
 from plotly.subplots import make_subplots
 
-from immo_rechner.core.profit_calculator import ProfitCalculator
+from immo_rechner.core.profit_calculator import ProfitCalculator, InputParameters
 import plotly.graph_objects as go
-
-
-def get_result(repayment_amount):
-    return ProfitCalculator.from_raw_data(
-        yearly_income=100_000,
-        monthly_rent=1500,
-        facility_monthly_cost=350.0,
-        owner_share=0.5,
-        repayment_amount=repayment_amount,
-        yearly_interest_rate=0.035,
-        initial_debt=450_000,
-        depreciation_rate=0.02,
-        purchase_price=500_000,
-    )
 
 
 def get_app():
     app = Dash()
+    app.title = "Immobilien Rechner"
     app.layout = [
         html.H1(children="Immobilien Rechner", style={"textAlign": "center"}),
         html.H2(
             children=[
-                "Repayment (lower)",
-                dcc.Textarea(id="repayment-value-lb", value="500.0"),
+                "Yearly income ",
+                dcc.Textarea(id="yearly-income", value="100000"),
             ]
         ),
         html.H2(
             children=[
-                "Repayment (upper)",
+                "(Repayment) upper ",
                 dcc.Textarea(id="repayment-value-up", value="2000.0"),
+                "lower ",
+                dcc.Textarea(id="repayment-value-lb", value="500.0"),
+                "step ",
+                dcc.Textarea(id="repayment-value-step", value="500.0"),
             ]
         ),
         html.H2(
             children=[
-                "Repayment (step)",
-                dcc.Textarea(id="repayment-value-step", value="500.0"),
+                "Monthly rent ",
+                dcc.Textarea(id="monthly-rent", value="1500.0"),
+                "Initial debt ",
+                dcc.Textarea(id="initial-debt", value="450000"),
             ]
         ),
         dcc.Graph(id="graph-cashflow"),
@@ -50,8 +43,18 @@ def get_app():
         Input("repayment-value-lb", "value"),
         Input("repayment-value-up", "value"),
         Input("repayment-value-step", "value"),
+        Input("yearly-income", "value"),
+        Input("monthly-rent", "value"),
+        Input("initial-debt", "value"),
     )
-    def update_graph(repayment_lb, repayment_ub, repayment_step):
+    def update_graph(
+        repayment_lb,
+        repayment_ub,
+        repayment_step,
+        yearly_income,
+        month_rent,
+        initial_debt,
+    ):
         repayment_lb = float(repayment_lb)
         repayment_ub = float(repayment_ub)
         step = float(repayment_step) if float(repayment_step) > 0 else 100
@@ -59,7 +62,20 @@ def get_app():
         fig = make_subplots(rows=2, cols=1)
 
         for repayment in np.arange(repayment_lb, repayment_ub, step):
-            df = get_result(repayment).simulate(n_years=10, to_pandas=True)
+            input_parameters = InputParameters(
+                yearly_income=yearly_income,
+                monthly_rent=month_rent,
+                facility_monthly_cost=350.0,
+                owner_share=0.5,
+                repayment_amount=repayment,
+                yearly_interest_rate=0.035,
+                initial_debt=initial_debt,
+                depreciation_rate=0.02,
+                purchase_price=500_000,
+            )
+            df = ProfitCalculator.from_raw_data(**input_parameters.dict()).simulate(
+                n_years=10, to_pandas=True
+            )
             fig.add_trace(
                 go.Scatter(x=df.year, y=df.cashflow, name=f"repayment: {repayment}"),
                 row=1,
