b931f3e9092156281d244dd39c02601b5562b53c
Soheil Hajian <soheil.hajian@outlook.com>
2024-09-01 16:33:43 +0200

    Adding a Dockerfile and docker-compose.yaml. Improving argument parsing for the app.

--- immo_rechner/app/app.py
+++ immo_rechner/app/app.py
@@ -1,6 +1,7 @@
 import os.path
 from typing import Iterable
 
+import click
 import numpy as np
 import plotly.graph_objects as go
 from dash import Dash, html, dcc, callback, Output, Input
@@ -123,6 +124,14 @@ def get_app():
     return app
 
 
-if __name__ == "__main__":
+@click.command()
+@click.option("--debug-mode", default=False, is_flag=True)
+@click.option("--host", default="127.0.0.1", type=str)
+@click.option("--port", default="8050", type=str)
+def main(debug_mode, host, port):
     app = get_app()
-    app.run(debug=True)
+    app.run(debug=debug_mode, host=host, port=port)
+
+
+if __name__ == "__main__":
+    main()
